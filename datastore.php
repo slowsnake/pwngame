94f25a396f330f4ffbe1d9fd4b075bd7faa24695
slowsnake <slowsnake36@gmail.com>
2015-10-20 15:08:04 +0530

    again sql

--- datastore.php
+++ datastore.php
@@ -4,26 +4,18 @@
 		$user_name=$_POST["username"];
 		$user_message=$_POST["message"];
 		$user_ip=$_POST["ipaddress"];
-		//var_dump($_POST);
-		
+				
+		$sql="insert into tbl_data1 (user_name,user_message,user_ip) values('".$user_name."','".$user_message."','".$user_ip."')";
+		echo $sql;		
+		if(mysqli_query($conn,$sql)
+			{
+			echo "Recorded successfully";
+		}
+		else
+		{
+			echo "Error: <br>" . mysqli_error($conn);
 
-
-		//echo "Hello" .$user_name ."<br> Thanks for sending us the message <br> $user_message <br> Your IP is marked as $user_ip";
-		
-		$sql="insert into tbl_data1 (user_name,user_message,user_ip) values('".$user_name."','".$user_message."','".$user_ip."');
-		echo "$sql";
-		exit("hi");
-		
-//		
-		//if(mysqli_query($conn,$sql)
-			//{
-			//echo "Recorded successfully";
-		//}
-		//else
-		//{
-			//echo "Error: <br>" . mysqli_error($conn);
-//
-		//}
-		//mysqli_close($conn);
-//	
+		}
+		mysqli_close($conn);
+	
 		?>
