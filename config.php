912761fb39d6c5cd125c8d75f4f542b807a19051
slowsnake <slowsnake36@gmail.com>
2015-10-20 13:35:44 +0530

    changed dbsettings

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 	$server_name="localhost";
 		$db_user="root"	;
-		$db_pass="gamerej447";
+		$db_pass="stupidpassword";
 		$db_name="learn1";
 
 		$conn = mysqli_connect($server_name,$db_user,$db_pass,$db_name);
